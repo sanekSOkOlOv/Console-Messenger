2d3af260d22cc402a446b4378c68ecc6dde37b55
sanekSOkOlOv <oleksandr.sokolov@nure.ua>
2022-06-02 14:22:26 +0300

    Создаю сервер

---
+++ MessangeServer/Controllers/MessangerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessangeServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessangerController : ControllerBase
+    {
+        // GET api/<MessangerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Id = " + id.ToString();
+        }
+
+        // POST api/<MessangerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
