da5bc0372a2bdc9fb5d4c6a99aad9a2f624708e5
sanekSOkOlOv <oleksandr.sokolov@nure.ua>
2022-06-02 14:11:21 +0300

    Добавьте файлы проекта.

---
+++ Messange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMessanger
+{
+        [Serializable]
+        public class Message
+        {
+            public string UserName { get; set; }
+            public string MessageText { get; set; }
+            public DateTime TimeStamp { get; set; }
+
+            public Message(string userName, string messageText, DateTime timeStamp)
+            {
+                UserName = userName;
+                MessageText = messageText;
+                TimeStamp = timeStamp;
+            }
+            public Message()
+            {
+                UserName = "System";
+                MessageText = "Online";
+                TimeStamp = DateTime.Now;
+            }
+
+            public override string ToString()
+            {
+                string output = String.Format("{0} <{1}> : {2}", UserName, TimeStamp, MessageText);
+                return output;
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleMessanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message ms = new Message();
+            string output = JsonConvert.SerializeObject(ms);
+            Message deserializedMs = JsonConvert.DeserializeObject<Message>(output);
+
+            Console.WriteLine(output);
+            Console.WriteLine(deserializedMs.ToString());
+
+            //{ "UserName":"System","MessageText":"Online","TimeStamp":"2022-06-02T12:21:54.219595+03:00"}
+            //System < 02.06.2022 12:21:54 > : Online
+        }
+    }
+}
+}
