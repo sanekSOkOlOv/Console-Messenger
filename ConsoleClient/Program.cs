b12d69b2131b4dac91874b9a163d7a2a07b03bdd
sanekSOkOlOv <oleksandr.sokolov@nure.ua>
2022-06-03 13:28:21 +0300

    Консольная версия клиента

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,21 +1,54 @@
 ﻿using System;
+using System.Threading;
 using Newtonsoft.Json;
 
 namespace ConsoleMessanger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Message ms = new Message();
-            string output = JsonConvert.SerializeObject(ms);
-            Message deserializedMs = JsonConvert.DeserializeObject<Message>(output);
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            //UserName = "Sokolov";
+            UserName = Console.ReadLine();
+            string MessageText = "";
 
-            Console.WriteLine(output);
-            Console.WriteLine(deserializedMs.ToString());
-
-            //{ "UserName":"System","MessageText":"Online","TimeStamp":"2022-06-02T12:21:54.219595+03:00"}
-            //System < 02.06.2022 12:21:54 > : Online
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
+
+        //Message ms = new Message();
+        //string output = JsonConvert.SerializeObject(ms);
+        //Message deserializedMs = JsonConvert.DeserializeObject<Message>(output);
+
+        //Console.WriteLine(output);
+        //Console.WriteLine(deserializedMs.ToString());
+
+        //{ "UserName":"System","MessageText":"Online","TimeStamp":"2022-06-02T12:21:54.219595+03:00"}
+        //System < 02.06.2022 12:21:54 > : Online
     }
 }
